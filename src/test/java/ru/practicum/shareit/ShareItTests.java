dca6f3f0edbfba16b8b66f1477a2efb7e1c73557
dmtereshchenko <116731368+dmtereshchenko@users.noreply.github.com>
2023-06-11 22:41:12 +0300

    Update ShareItTests.java

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,16 +1,9 @@
 package ru.practicum.shareit;
 
-import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.booking.service.BookingService;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.service.ItemService;
-import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.service.UserService;
 
 @SpringBootTest
-@AllArgsConstructor
 class ShareItTests {
 
 	@Test
