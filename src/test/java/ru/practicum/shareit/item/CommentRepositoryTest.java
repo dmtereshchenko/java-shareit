373985ff7639e0799125682b7a09f75137a5f7e9
dmtereshchenko <tereshchenkodm@yandex.ru>
2023-06-29 00:40:48 +0300

    Версия 15.1.1 правки codestyle

---
+++ src/test/java/ru/practicum/shareit/item/CommentRepositoryTest.java
@@ -0,0 +1,29 @@
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.storage.CommentRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CommentRepositoryTest {
+
+    @Mock
+    private CommentRepository commentRepository;
+    Comment comment = new Comment(1L, "commentText", null, null, LocalDateTime.now());
+
+    @Test
+    void findCommentsByItemIdTest() {
+        when(commentRepository.findCommentsByItemId(anyList())).thenReturn(List.of(comment));
+        assertEquals(List.of(comment), commentRepository.findCommentsByItemId(List.of(1L)));
+    }
+}
